0f5da1f22878394cdf0cf9cb9a34579d0a852d9f
Joe Krystofik <jkrystofik2290@hotmail.com>
2017-06-28 12:17:44 +0000

    Update 6/28/2017

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/client/index.html');
+});
+
+//could be used to create a site visit counter
+io.on('connection', function(socket){
+  console.log('a user connected');
+  
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
 });
 
 http.listen(process.env.PORT, process.env.IP, function(){

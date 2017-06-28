0408f79b12dd7c71f81232e93892b49be7314250
Joe Krystofik <jkrystofik2290@hotmail.com>
2017-06-28 18:33:20 +0000

    Update 6/28/2017

--- index.js
+++ index.js
@@ -2,20 +2,43 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var room =[];
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/client/index.html');
 });
 
 //could be used to create a site visit counter
 io.on('connection', function(socket){
-  console.log('a user connected');
   
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    //add username disconnected
+    for(var i=0; i<room.length; i++){
+      if(socket.id == room[i].id){
+        socket.broadcast.emit('chat message', room[i].name + " has left the chatroom!");
+        console.log('user: '+ room[i].name +' disconnected');
+        room.splice(i,1);
+        break;
+      }
+    }
   });
   
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    for(var i=0; i<room.length; i++){
+      if(socket.id == room[i].id){
+        io.emit('chat message', room[i].name + ': ' + msg);
+        break;
+      }
+    }
+  });
+  
+  socket.on('add-user', function(user){
+    if(user == null || user == ''){
+      user = "Mr.NoName";
+    }
+    room.push({"id": socket.id, "name": user});
+    socket.broadcast.emit('chat message', user + " has entered the chatroom!");
+    console.log('user: ' + user + ' has connected');
   });
 });
 
